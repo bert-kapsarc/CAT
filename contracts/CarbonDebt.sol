5f4b77ab54cbd5d9180c87bcc0e8769ef57eab82
brioux <bertrand.rioux@gmail.com>
2019-12-17 09:24:29 +0300

    MultisigWallet submodule: https://github.com/gnosis/MultiSigWallet.git
    Added MultisigWallet submodule and TO-DO. Create multisig wallet to confim
    - receiveDebt transaction request
    - sendDebt transaction request
    - Can use eighter 2of2 of NofM multisig.

--- contracts/CarbonDebt.sol
+++ contracts/CarbonDebt.sol
@@ -1,46 +1,121 @@
 pragma solidity ^0.5.12;
-contract CarbonDebt {
 
-    enum PlayerClass { Producer, Consumer, Manager}
+library Participant{
+    enum Class { Default, Producer, Consumer, Sinker}
+}
 
-    struct Player {
-        PlayerClass class;
-        address[] backers;
-        uint debt; //integer value of carbon debt in kg.
+contract Directory{
+    address public owner; //owner of the directory contract
+    address[] public contracts; //list of all contract addresses in the directory
+    constructor() public {
+        owner = msg.sender;
     }
 
-    address public contract_owner;
+    event newDebtContract(
+        address contractAddress
+    );
 
-    mapping(address => Player) public players;
+    function createContract(Participant.Class participant_class, uint debt) external payable returns(address newContract) {
+        // this function creates new debt contract
+        // uses the default debt contract logic developed for this project
+        // the registerContrsact can be used to register external contracts with different logic
+        debtContract c = (new debtContract).value(msg.value)(address(msg.sender), participant_class, debt);
+        contracts.push(address(c));
+        emit newDebtContract(address(c));
+        return address(c);
+    }
 
-    constructor() public {
-        contract_owner = msg.sender;
+    function registerContract(address c) external returns(address newContract){
+        // this function is used to register an existing external debt contract. 
+        // This way participants can publish new contracts external to the default hard coded into createContract
+        // TODO add some validation checks inorder for the external contract to be accepted into the directory
+        //  i.  
+        //  ii. 
+        contracts.push(c);
+        emit newDebtContract(c);
+        return c;
+    }
+}
+
+contract debtContract{
+    struct Member {
+        Participant.Class class;
+        address[] backers;
+        uint debt; //integer value of carbon debt in kg.
+        bool exists;
+    }
+
+    address public owner;
+
+    mapping(address => Member) public participants;
+
+    constructor(address owner_address, Participant.Class owner_class, uint carbon) public payable {
+        owner = owner_address;
+        participants[owner_address].class = owner_class;
+        participants[owner_address].debt += carbon;
     }  
 
-    function createNewPlayer(PlayerClass player_class, uint carbon) public {
-        // only create player if its class has not yet been set
-        require(uint(players[msg.sender].class)==0);
-        players[msg.sender].class = player_class;
+    function createNewParticipant(Participant.Class participant_class, uint carbon) public {
+        // only create participant if has not yet been set
+        require(participants[msg.sender].exists);
+        participants[msg.sender].class = participant_class;
         assignDebt(carbon);
     }
 
     function assignDebt(uint carbon) public {
-        require(uint(players[msg.sender].class)>0);
-        players[msg.sender].debt += carbon;
+        // only the contract owner can assign debt to their own Participant mapping
+        require(
+            msg.sender==owner,
+            "Only the contract owner can assign debt to himself"
+        );
+        //require(uint(participants[msg.sender].class)>0);
+        participants[msg.sender].debt += carbon;
     }
 
     function receiveDebt(address source, uint carbon) external {
-        require(players[source].debt>carbon);
-        players[source].debt -= carbon;
-        players[msg.sender].debt += carbon;
+        // any address can request owner to send them debt
+
+        // TO-DO - add mutisig functionality to prevent debt trolling
+        // Propose using the MultiSigWallet from https://github.com/gnosis/MultiSigWallet/releases
+        // Code cloned into git submodule MultiSigWallet
+        
+        // The multisig wallet can be funded to send ether
+        // however, we may only want the wallet to send confirmtion that releases the debt
+        // This requires both parties in the debt transfer to approve it (2of2 multisig)
+        // Can use the Transaction.data stored in the multisig wallet to trigger debt transfer when the multisig is confirmed 
+        // and executed via the wallets external_call function
+
+        // Note: The multisig allows us to assocaite multiple public/private keys 
+        // to a given debt transfer transaction request
+        // and enable NofM multisig to confirm a transaciotn 
+
+        // Example contract confirmation protocol
+        // Receiver opens a receiveDebt tx request by sending a commitment to pay for energy/product, 
+        // Note the receiver could fund the MultisigWallet 
+        // Embeds confirmation of payment within contract logic
+        // Opon receipt of payment bebt owner signs tx request to releases debt 
+        // and the contract reflects the commitment to sell.
+
+
+        require(
+            participants[source].debt>carbon,
+            'Debt transfer exceeds debt balance'
+        );
+        participants[source].debt -= carbon;
+        participants[msg.sender].debt += carbon;
     }
 
-    function playerDebt(address _player) public view returns (uint){
-        return players[_player].debt;
+    function sendDebt(address source, uint carbon) external {
+        // Used by debt owners to sned debt to another account
+        // Again this requires using the multisig wallet.
     }
 
-    function playerType(address _player) public view returns (uint){
-        return uint(players[_player].class);
+    function participantDebt(address _participant) public view returns (uint){
+        return participants[_participant].debt;
+    }
+
+    function participantType(address _participant) public view returns (uint){
+        return uint(participants[_participant].class);
     }
 
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,4 +2,5 @@ const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(CarbonDebt, 1, 100);
 };
